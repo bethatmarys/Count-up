1fc92d30a5d39d5cb1f1edc6251f621bbff77f5b
beth hansen and phat nguyen <beth@>
2017-05-02 11:24:16 -0700

    Count-up unfinish

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+
+$(document).ready(function() {
+  $("form#form-group").submit(function(event) {
+    var number1 = parseInt($("input#numberone").val());
+    var number2 = parseInt($("input#numbertwo").val());
+// debugger;
+    if (!(number1) || !(number2)){
+      alert("Please enter a number");
+    }
+    else if ((number1 < 0) || (number2 < 0)) {
+      alert ("Please enter a postive number");
+    }
+    else if ((number1 = "") || (number2 = "")) {
+      alert ("Please enter a ? number");
+    }
+
+    var outputs = [];
+
+    var total = 0;
+    for (var currentNumber = 0; currentNumber <= number1; currentNumber += number2) {
+      total += number2 ;
+      outputs.push(total);
+    }
+     $("p").append("your output of : " + " " + output + ".");
+
+
+
+    event.preventDefault();
+  });
+  });
